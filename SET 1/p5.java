2ee3ad4a451d92893af6e776c9e98b7cf8f605e7
Himesh Chauhan <136347534+himeshchohan@users.noreply.github.com>
2026-01-28 19:54:17 +0530

    Add files via upload

---
+++ SET 1/p5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class p5{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter side a: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter side b: ");
+        double b = sc.nextDouble();
+        System.out.print("Enter side c: ");
+        double c = sc.nextDouble();
+
+        if (a+b > c && a+c > b && b+c > a) {
+            double s = (a+b+c) / 2;
+            double area = Math.sqrt(s * (s-a) * (s-b) * (s-c));
+            System.out.print("The area of the triangle is: " +area);
+        }
+        else {
+            System.out.println("The given sides do not form a valid triangle.");
+        }
+    }
+}
