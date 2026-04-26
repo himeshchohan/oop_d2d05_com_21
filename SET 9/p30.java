81963de579d306910c5e0400de893e85e08ee1cd
Himesh Chauhan <136347534+himeshchohan@users.noreply.github.com>
2026-04-26 10:43:37 +0530

    Add files via upload

---
+++ SET 9/p30.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.*;
+public class p30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String input = sc.nextLine();
+        input = input.toLowerCase();
+        String[] words = input.split("\\s+");
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } 
+            else {
+                map.put(word, 1);
+            }
+        }
+        System.out.println("\nWord Frequencies:");
+        for (String word : map.keySet()) {
+            System.out.println(word + " -> " + map.get(word));
+        }
+    }
+}
